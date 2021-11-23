ed38448d090e6043898a636e7b96aacc68f64b58
Mohit <mohitrawal3131@gmail.com>
2021-11-22 20:05:23 -0500

    Security Updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,19 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAWQ9z8BjYdHBTMVpDj72ZoIlzllrnlwEo",
-    authDomain: "twitter-clone-643c4.firebaseapp.com",
-    projectId: "twitter-clone-643c4",
-    storageBucket: "twitter-clone-643c4.appspot.com",
-    messagingSenderId: "634515777365",
-    appId: "1:634515777365:web:b50a6b669f47a3cbf4b1dc"
+    // apiKey: "AIzaSyAWQ9z8BjYdHBTMVpDj72ZoIlzllrnlwEo",
+    // authDomain: "twitter-clone-643c4.firebaseapp.com",
+    // projectId: "twitter-clone-643c4",
+    // storageBucket: "twitter-clone-643c4.appspot.com",
+    // messagingSenderId: "634515777365",
+    // appId: "1:634515777365:web:b50a6b669f47a3cbf4b1dc"
 
-    // apiKey: process.env.REACT_APP_apiKey,
-    // authDomain: process.env.REACT_APP_authDomain,
-    // projectId: process.env.REACT_APP_projectId,
-    // storageBucket: process.env.REACT_APP_storageBucket,
-    // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    // appId: process.env.REACT_APP_appId
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 const app = firebase.initializeApp(firebaseConfig);
